9008a1c857a069fc38145f866cf7e45cec75a2f5
Skoomaki <skoomaki12@gmail.com>
2023-04-01 16:19:14 +0100

    feature: added folder structure for index and css

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import '../../styles/login/login.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          RotaFlex
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
